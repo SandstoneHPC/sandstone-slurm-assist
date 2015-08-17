ea94e2b86e37176f16c4079e359a2a919f8f0ec1
Zeb Sampedro <sampedro@colorado.edu>
2015-08-17 16:27:27 -0600

    More meaningful URL schema.

--- oideslurm/urls.py
+++ oideslurm/urls.py
@@ -6,6 +6,6 @@ from oideslurm.handlers import JobHandler
 
 URL_SCHEMA = [
             (r"/slurm/a/config", FormConfigHandler),
-            (r"/slurm/a/job", JobListHandler),
+            (r"/slurm/a/jobs", JobListHandler),
             (r"/slurm/a/job/?(?P<jobid>[0-9]+)?", JobHandler),
         ]
