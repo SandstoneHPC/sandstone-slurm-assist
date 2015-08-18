ce6381cb97ba70943de8a3dbfd3a536b4fd15c94
Zeb Sampedro <sampedro@colorado.edu>
2015-08-17 18:40:34 -0600

    Stubbed in an sacct command method.

--- oideslurm/mixins/slurm_mixin.py
+++ oideslurm/mixins/slurm_mixin.py
@@ -1,8 +1,15 @@
 import tornado.web
+import subprocess
 
 
 
 class SlurmCmdMixin(tornado.web.RequestHandler):
     # This class will contain any methods necessary
     # for running slurm commands and parsing output.
-    pass
+    def run_sacct(self,**kwargs):
+        # Handle keyword arguments, translate them into
+        # corresponding options for sacct via subprocess
+        options = ['-P']
+        cmd = ['sacct'] + options
+        cmd_out = subprocess.check_output(cmd)
+        # Parse output and return
