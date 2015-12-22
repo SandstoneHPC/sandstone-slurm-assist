674e68e0cfe63d95a6c8346129e92c65d9b8b534
kansuke231 <ikeharakansuke@gmail.com>
2015-12-22 11:22:08 -0700

    Wrote basic scripts for e2e testing.

---
+++ e2e-tests/environment.js
@@ -0,0 +1,8 @@
+module.exports = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  baseUrl: 'http://localhost:8888',
+  creds: {
+    username: '',
+    password: ''
+  }
+};
---
+++ e2e-tests/protractor.conf.js
@@ -0,0 +1,43 @@
+var env = require('./environment.js');
+
+exports.config = {
+  allScriptsTimeout: 11000,
+
+  seleniumAddress: env.seleniumAddress,
+
+  specs: [
+    '*.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: env.baseUrl,
+
+  framework: 'jasmine2',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  },
+
+  params: {
+    login: {
+      username: env.creds.username,
+      password: env.creds.password
+    }
+  },
+
+  onPrepare: function() {
+    browser.driver.get(env.baseUrl+'/auth/login?next=%2F#/slurm');
+    browser.driver.findElement(by.css('input[name=username]')).sendKeys(env.creds.username);
+    browser.driver.findElement(by.css('input[name=password]')).sendKeys(env.creds.password);
+    browser.driver.findElement(by.css('.form-signin > button')).click();
+
+    return browser.driver.wait(function() {
+      return browser.driver.getCurrentUrl().then(function(url) {
+        return /editor/.test(url);
+      });
+    }, 10000, "URL hasn't changed.");
+  }
+};
