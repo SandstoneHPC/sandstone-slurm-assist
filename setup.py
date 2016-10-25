e0a555edf48c8711229305356aeeb4049295d546
Zebula Sampedro <sampedro@colorado.edu>
2016-10-25 11:20:05 -0600

    Bumped version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='sandstone-slurm-assist',
-    version='0.9.3',
+    version='0.9.5',
     author=u'Zebula Sampedro',
     author_email='sampedro@colorado.edu',
     packages=find_packages(),
@@ -15,7 +15,7 @@ setup(
     long_description=open('DESCRIPTION.rst').read(),
     zip_safe=False,
     install_requires=[
-        'sandstone==0.12.5',
+        'sandstone>=0.12.5',
         'jsonschema>=2.5.1',
     ],
     classifiers=[
