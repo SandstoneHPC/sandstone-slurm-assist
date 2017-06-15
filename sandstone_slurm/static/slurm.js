91d5d09f53cd31295f206ac93b8d648ae62822be
Zebula Sampedro <sampedro@colorado.edu>
2017-06-15 16:11:32 -0600

    Made status view the landing page.

--- sandstone_slurm/static/slurm.js
+++ sandstone_slurm/static/slurm.js
@@ -2,27 +2,11 @@
 
 angular.module('sandstone.slurm', ['ui.bootstrap','ui.ace','smart-table','ui.router'])
 
-.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-
+.config(['$stateProvider', '$urlRouterProvider' ,function($stateProvider, $urlRouterProvider){
   $stateProvider.state('slurm', {
-    'url': '/slurm',
-    'views': {
-      '': {
-        templateUrl: '/static/slurm/slurm.html'
-      },
-      'schedule@slurm': {
-        templateUrl: '/static/slurm/templates/schedule.html',
-        controller: 'ScheduleCtrl as ctrl',
-        resolve: {
-          formConfig: function(ScheduleService) {
-            return ScheduleService.loadFormConfig();
-          }
-        }
-      },
-      'status@slurm': {
-        templateUrl: '/static/slurm/templates/status.html',
-        controller: 'StatusCtrl as ctrl'
-      }
-    }
-  });
-}])
+      url: '/slurm',
+      templateUrl: '/static/slurm/slurm.html',
+      controller: 'StatusCtrl',
+      controllerAs: 'ctrl'
+    });
+}]);
